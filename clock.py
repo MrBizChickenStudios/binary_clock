78ab11288b0bcd587a510563f3bf980fd25db13a
Jordan Dodson <jordandodson@Jordans-Air.lan>
2025-03-18 16:06:29 -0500

    fixed clock finished clock

--- clock.py
+++ clock.py
@@ -1,3 +1,5 @@
+import pygame
+from datetime import datetime
 from constants import *
 import light
 import time
@@ -14,112 +16,73 @@ class Clock():
         self.tenth_hours = []
 
         for i in range(1, 5):
-            self.seconds.append(light.Light(PADDING * 9 + RADIUS * 9, PADDING * i + RADIUS * i, 32))
+            self.seconds.append(light.Light(PADDING * 9 + RADIUS * 9, GAME_HEIGHT - (PADDING * i + RADIUS * i), 32))
+
         for i in range(1, 4):
-            self.tenth_seconds.append(light.Light(PADDING * 8 + RADIUS * 8, PADDING * i + RADIUS * i, 32))
+            self.tenth_seconds.append(light.Light(PADDING * 8 + RADIUS * 8, GAME_HEIGHT - (PADDING * i + RADIUS * i), 32))
+
         for i in range(1, 5):
-            self.minutes.append(light.Light(PADDING * 6 + RADIUS * 6, PADDING * i + RADIUS * i, 32))
+            self.minutes.append(light.Light(PADDING * 6 + RADIUS * 6, GAME_HEIGHT - (PADDING * i + RADIUS * i), 32))
+
         for i in range(1, 4):
-            self.tenth_minutes.append(light.Light(PADDING * 5 + RADIUS * 5, PADDING * i + RADIUS * i, 32))
+            self.tenth_minutes.append(light.Light(PADDING * 5 + RADIUS * 5, GAME_HEIGHT - (PADDING * i + RADIUS * i), 32))
+
         for i in range(1, 5):
-            self.hours.append(light.Light(PADDING * 3 + RADIUS * 3, PADDING * i + RADIUS * i, 32))
-        for i in range(1, 3):
-            self.tenth_hours.append(light.Light(PADDING * 2 + RADIUS * 2, PADDING * i + RADIUS * i, 32))
+            self.hours.append(light.Light(PADDING * 3 + RADIUS * 3, GAME_HEIGHT - (PADDING * i + RADIUS * i), 32))
 
         for i in range(1, 3):
-            self.blinker_1.append(light.Light(PADDING * 4 + RADIUS * 4, PADDING * i + RADIUS * i + 100, 20))
+            self.tenth_hours.append(light.Light(PADDING * 2 + RADIUS * 2, GAME_HEIGHT - (PADDING * i + RADIUS * i), 32))
 
         for i in range(1, 3):
-            self.blinker_2.append(light.Light(PADDING * 7 + RADIUS * 7, PADDING * i + RADIUS * i + 100, 20))
+            self.blinker_1.append(light.Light(PADDING * 4 + RADIUS * 4, GAME_HEIGHT - (PADDING * i + RADIUS * i + 100), 20))
 
+        for i in range(1, 3):
+            self.blinker_2.append(light.Light(PADDING * 7 + RADIUS * 7, GAME_HEIGHT - (PADDING * i + RADIUS * i + 100), 20))
 
     def update(self):
-        # Get current time
-        current_time = time.localtime()
-        seconds = current_time.tm_sec
-        minutes = current_time.tm_min
-        hours = current_time.tm_hour
+        now = datetime.now()
+        current_time = now.strftime("%H%M%S")
 
-        for light in self.seconds:
-            light.update()
+        time = []
+        for t in current_time:
+            time.append(int(t))
 
-        for light in self.tenth_seconds:
-            light.update()
+        for light_set in [
+            self.seconds, self.tenth_seconds, self.minutes,
+            self.tenth_minutes, self.hours, self.tenth_hours,
+            self.blinker_1, self.blinker_2
+        ]:
+            for light in light_set:
+                light.update()
 
-        for light in self.minutes:
-            light.update()
+        self.show_lights(self.seconds, time[5])
+        self.show_lights(self.tenth_seconds, time[4])
+        self.show_lights(self.minutes, time[3])
+        self.show_lights(self.tenth_minutes, time[2])
+        self.show_lights(self.hours, time[1])
+        self.show_lights(self.tenth_hours, time[0])
 
-        for light in self.tenth_minutes:
-            light.update()
-
-        for light in self.hours:
-            light.update()
-
-        for light in self.tenth_hours:
-            light.update()
-
-        for light in self.blinker_1:
-            light.update()
-
-        for light in self.blinker_2:
-            light.update()
-
-
-        self.show_lights(self.seconds, seconds // 4)
-        self.show_lights(self.tenth_seconds, seconds % 4)
-
-        self.show_lights(self.minutes, minutes // 4)
-        self.show_lights(self.tenth_minutes, minutes % 4)
-
-        self.show_lights(self.hours, hours // 4)
-        self.show_lights(self.tenth_hours, hours % 4)
-
-        self.show_lights(self.blinker_1,0)
-        self.show_lights(self.blinker_2, 0)
 
     def draw(self, surface):
-        for second in self.seconds:
-            second.draw(surface)
-
-        for tenth_second in self.tenth_seconds:
-            tenth_second.draw(surface)
-
-        for minute in self.minutes:
-            minute.draw(surface)
-
-        for tenth_minute in self.tenth_minutes:
-            tenth_minute.draw(surface)
-
-        for hour in self.hours:
-            hour.draw(surface)
-
-        for tenth_hour in self.tenth_hours:
-            tenth_hour.draw(surface)
-
-        for light in self.blinker_1:
-            light.draw(surface)
-
-        for light in self.blinker_2:
-            light.draw(surface)
+        for light_set in [
+            self.seconds, self.tenth_seconds, self.minutes,
+            self.tenth_minutes, self.hours, self.tenth_hours,
+            self.blinker_1, self.blinker_2
+        ]:
+            for light in light_set:
+                light.draw(surface)
 
     def show_lights(self, lights, num):
-        bit = [8, 4, 2, 1]
-        output = []
+        bit_length = len(lights)
+        bit_values = [2**i for i in range(bit_length)][::-1]
+        output = [0] * bit_length
 
-        for b in bit:
-            if num >= b:
-                output.append(1)
-                num -= b
-            else:
-                output.append(0)
+        for i in range(bit_length):
+            if num >= bit_values[i]:
+                output[i] = 1
+                num -= bit_values[i]
 
+        output.reverse()
 
-        output = output[:len(lights)]
-
-        for i in range(len(output)):
-            if output[i] == 1:
-                lights[i].on = True
-            else:
-                lights[i].on = False
-
-            
+        for i in range(bit_length):
+            lights[i].on = bool(output[i])
